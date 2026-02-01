d9948c342eed47d1ff3b797f130b3b3a9619e6cc
Raymond <raymondyang59@gmail.com>
2026-02-01 10:42:39 -0800

    Restructured the app

---
+++ CmsBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsBackend.Data;
+using CmsBackend.Models;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+    public DbSet<Article> Articles => Set<Article>();
+}
